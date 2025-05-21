3431cf452cfc340682c1c92075973f57b6dc7920
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-05-21 21:17:00 +0530

    seller product  integration

--- frontend/src/pages/seller/AddProduct.jsx
+++ frontend/src/pages/seller/AddProduct.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { assets, categories } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const AddProduct = () => {
 
@@ -10,9 +12,40 @@ const AddProduct = () => {
     const [price, setPrice] = useState('');
     const [offerPrice, setOfferPrice] = useState('');
 
-    const onSubmitHandler = (e) => {
-        e.preventDefault();
-       
+    const {axios} = useAppContext();
+
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault();
+            const productData = {
+                name,
+                description : description.split("\n"),
+                category,   
+                price,
+                offerPrice,
+            }
+            const formData = new FormData();
+            formData.append('productData', JSON.stringify(productData));
+            for(let i = 0; i < files.length; i++){
+                formData.append('image', files[i]);
+            }
+
+        const {data} = await axios.post('/api/seller/product/add', formData);
+        if(data.success){
+            toast.success(data.message);
+            setName("");
+            setDescription("");
+            setCategory("");
+            setPrice("");
+            setOfferPrice("");
+            setFiles([]);
+        }  
+        else{
+            toast.error(data.message);
+        }
+        } catch (error) {
+            toast.error(error.message);
+        }            
 
     }
 
